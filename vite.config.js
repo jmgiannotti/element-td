cf0085f6022c5a3d9cbe95298d321fa94e9469bc
jmgiannotti <juanmartingiannotti@gmail.com>
2026-08-16 01:06:50 -0300

    vite build to split phaser into a separate chunk

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser'],
+        },
+      },
+    },
+  },
+});
